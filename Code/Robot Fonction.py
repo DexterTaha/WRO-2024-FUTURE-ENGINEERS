75401d012d4711627c856984cadac14db5609586
Salmane Derdeb <107891610+salmane-derdeb@users.noreply.github.com>
2024-06-04 17:44:48 +0100

    Update Robot Fonction.py

--- Code/Robot Fonction.py
+++ Code/Robot Fonction.py
@@ -1,3 +1,9 @@
+from pybricks.hubs import InventorHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+
 
 # Register the standard input so we can read keyboard presses.
 keyboard = poll()
